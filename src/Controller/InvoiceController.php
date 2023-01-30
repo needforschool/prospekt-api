71db95fba7d3ee1b0a958106868bc5f26b9e33b2
MaximeC <maxime.carville@gmail.com>
2023-01-30 14:58:31 +0100

    first create invoice

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Invoice;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InvoiceController extends AbstractController
+{
+    #[Route('/invoice', name: 'app_invoice')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/InvoiceController.php',
+        ]);
+    }
+    #[Route('/invoiceCreate', name: 'app_invoice', methods: ['POST'])]
+    public function createInvoice(Request $request,ManagerRegistry $doctrine): Response {
+        $entityManager = $doctrine->getManager();
+
+        $invoice = new invoice();
+
+        $data = json_decode($request->getContent(), true);
+
+        $user = $doctrine->getRepository(User::class)->find($data['idCustomer']);
+
+        $invoice->setCustomerId($user)
+                ->setStatus($data['name'])
+                ->setCreatedAt(new \DateTimeImmutable());
+
+        $entityManager->persist($invoice);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'success'], Response::HTTP_OK);
+
+    }
+}
